e9c7ffc8019003bd6e567a9f5e82a29f406f26bb
AlexPeile <47165032+AlexPeile@users.noreply.github.com>
2020-11-17 20:48:02 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,12 +18,12 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Project information -----------------------------------------------------
 
-project = 'EyeLock'
+project = 'TeachTrade'
 copyright = '2020, Alexander Peile'
 author = 'Alexander Peile'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
